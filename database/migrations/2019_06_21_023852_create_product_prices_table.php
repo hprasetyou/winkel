c6f73ace42a9e40163d81d358d321c004cd24176
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-06-21 09:49:03 +0700

    basic table structure

---
+++ database/migrations/2019_06_21_023852_create_product_prices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductPricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_prices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('product_id');
+            $table->string('name');
+            $table->boolean('main');
+            $table->timestamp('start_time')->nullable();
+            $table->timestamp('end_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_prices');
+    }
+}
