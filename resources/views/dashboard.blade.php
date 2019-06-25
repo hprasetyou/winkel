3b52ea998712674e05ae1fb5e48bc7d022d1c329
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-06-25 09:21:22 +0700

    move nav layout to new file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,26 +13,7 @@
 <body>
     <div id="app">
         <v-app id="inspire">
-            <v-navigation-drawer fixed v-model="drawer" app>
-                <v-list dense>
-                    <v-list-tile @click="">
-                        <v-list-tile-action>
-                            <v-icon>home</v-icon>
-                        </v-list-tile-action>
-                        <v-list-tile-content>
-                            <v-list-tile-title>Home</v-list-tile-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                    <v-list-tile @click="">
-                        <v-list-tile-action>
-                            <v-icon>contact_mail</v-icon>
-                        </v-list-tile-action>
-                        <v-list-tile-content>
-                            <v-list-tile-title>Contact</v-list-tile-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </v-list>
-            </v-navigation-drawer>
+            @include('layout.navigation')
             <v-toolbar color="indigo" dark fixed app>
                 <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
                 <v-toolbar-title>Application</v-toolbar-title>
